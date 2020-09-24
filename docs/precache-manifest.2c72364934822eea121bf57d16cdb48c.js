b3bdb39ead5a9b06680e4c7a2401fb7f2d65f801
Anyungu <anyungucw@gmail.com>
2020-09-24 19:34:50 +0300

    first

---
+++ docs/precache-manifest.2c72364934822eea121bf57d16cdb48c.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0594409386689e6b5061d3552c7a040",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "37e46113e676ed04098d",
+    "url": "/portfolio/static/css/3.f9898e9a.chunk.css"
+  },
+  {
+    "revision": "41ea46c78e5b7cb1dd4a",
+    "url": "/portfolio/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "b2afa10d09bb395df1bd",
+    "url": "/portfolio/static/css/main.b7651eea.chunk.css"
+  },
+  {
+    "revision": "e9a5c708e7b59708d08c",
+    "url": "/portfolio/static/js/2.09d480f7.chunk.js"
+  },
+  {
+    "revision": "71772b085033e92f89cdfb3071333458",
+    "url": "/portfolio/static/js/2.09d480f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37e46113e676ed04098d",
+    "url": "/portfolio/static/js/3.e78c12ae.chunk.js"
+  },
+  {
+    "revision": "41ea46c78e5b7cb1dd4a",
+    "url": "/portfolio/static/js/4.ece5aab5.chunk.js"
+  },
+  {
+    "revision": "b2afa10d09bb395df1bd",
+    "url": "/portfolio/static/js/main.8d93d7ae.chunk.js"
+  },
+  {
+    "revision": "f36bee785ef60eb9d7cf",
+    "url": "/portfolio/static/js/runtime-main.40dc7375.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/portfolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/portfolio/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "b90f93962225e8652a1f3474cd77d80b",
+    "url": "/portfolio/static/media/dctLogo.b90f9396.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/portfolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/portfolio/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/portfolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/portfolio/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "0288c5ec7a87ea73f8d14b12fc4bc94e",
+    "url": "/portfolio/static/media/talksCardBack.0288c5ec.svg"
+  }
+]);
---
+++ src/portfolio.js
@@ -0,0 +1,269 @@
+﻿
+/* Change this file to get your personal Portfolio */
+
+// Your Summary And Greeting Section
+
+import emoji from "react-easy-emoji";
+
+const greeting = {
+  /* Your Summary And Greeting Section */
+  username: "Anyungu Wanyungu",
+  title: "Hi all, I'm Anyungu",
+  subTitle: emoji("I am an agile Full Stack Engineer 🚀 having actual experience in designing, building and deploying scalable apps in  Spring Boot, Node Js and React. I learn fast! I have worked outside my stack"),
+  resumeLink: "https://drive.google.com/file/d/1VT-kQSP1zhF9xOuK-43ozrJER6v3S3m4/view?usp=sharing"
+};
+
+// Your Social Media Link
+
+const socialMediaLinks = {
+  github: "https://github.com/adams-okode",
+  linkedin: "https://www.linkedin.com/in/adams-okode-a3b4b366/",
+  gmail: "adamsokode@gmail.com",
+  gitlab: "https://gitlab.com/adams_okode",
+  facebook: "https://web.facebook.com/dan.aunndah/"
+  // Instagram and Twitter are also supported in the links!
+};
+
+// Your Skills Section
+const skillsSection = {
+  title: "What i do",
+  subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  skills: [
+    // emoji("⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"),
+    // emoji("⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"),
+    // emoji("⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean")
+  ],
+
+  /* Make Sure You include correct Font Awesome Classname to view your icon
+  https://fontawesome.com/icons?d=gallery */
+
+  softwareSkills: [
+    {
+      skillName: "Java (Spring Boot)",
+      fontAwesomeClassname: "fab fa-java"
+    },
+    {
+      skillName: "Python (Django)",
+      fontAwesomeClassname: "fab fa-python"
+    },
+    {
+      skillName: "PHP (Laravel)",
+      fontAwesomeClassname: "fab fa-php"
+    },
+    {
+      skillName: "nodejs",
+      fontAwesomeClassname: "fab fa-node"
+    },
+    {
+      skillName: "sql-database",
+      fontAwesomeClassname: "fas fa-database"
+    },
+    {
+      skillName: "no-sql-database",
+      fontAwesomeClassname: "fas fa-database"
+    },
+    {
+      skillName: "Angular",
+      fontAwesomeClassname: "fab fa-angular"
+    },
+    {
+      skillName: "AWS",
+      fontAwesomeClassname: "fab fa-aws"
+    },
+    {
+      skillName: "Digital Ocean",
+      fontAwesomeClassname: "fab fa-digital-ocean"
+    },
+    {
+      skillName: "Docker",
+      fontAwesomeClassname: "fab fa-docker"
+    },
+    {
+      skillName: "Kubernetes",
+      fontAwesomeClassname: "fab fa-cubes"
+    }
+  ]
+};
+
+// Your top 3 proficient stacks/tech experience
+
+const techStack = {
+  viewSkillBars: false, //Set it to true to show Proficiency Section
+  experience: [
+    {
+      Stack: "Frontend/Design",  //Insert stack or technology you have experience in
+      progressPercentage: "70%"  //Insert relative proficiency in percentage
+    },
+    {
+      Stack: "Backend",
+      progressPercentage: "99%"
+    },
+    {
+      Stack: "Programming",
+      progressPercentage: "60%"
+    }
+  ]
+};
+
+
+// Your top 3 work experiences
+
+const workExperiences = {
+  viewExperiences: true, //Set it to true to show workExperiences Section
+  experience: [
+    {
+      role: "Software Engineer (IoT)",
+      company: "EED Advisory",
+      companylogo: require("./assets/images/eed-logo.png"),
+      date: "November 2018 – Present",
+      desc: "",
+      descBullets: [
+        // "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        // "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+      ]
+    },
+    {
+      role: "Software Developer",
+      company: "Directcore Technologies",
+      companylogo: require("./assets/images/dctLogo.png"),
+      date: "August 2018 – November 2018",
+      desc: ""
+    },
+    {
+      role: "Software Developer Intern",
+      company: "Directcore Technologies",
+      companylogo: require("./assets/images/dctLogo.png"),
+      date: "June 2015 – August 2018",
+      desc: ""
+    },
+  ]
+};
+
+/* Your Open Source Section to View Your Github Pinned Projects
+To know how to get github key look at readme.md */
+
+const openSource = {
+  githubConvertedToken: process.env.REACT_APP_GITHUB_TOKEN,
+  githubUserName: "adams-okode", // Change to your github username to view your profile in Contact Section.
+  showGithubProfile: "true" // Set true or false to show Contact profile using Github, defaults to false 
+};
+
+
+// Some Big Projects You have worked with your company
+
+const bigProjects = {
+  title: "Big Projects",
+  subtitle: "SOME STARTUPS AND COMPANIES THAT I HELPED TO CREATE THEIR TECH",
+  projects: [
+    {
+      image: require("./assets/images/saayaHealthLogo.webp"),
+      link: "http://saayahealth.com/"
+    },
+    {
+      image: require("./assets/images/nextuLogo.webp"),
+      link: "http://nextu.se/"
+    }
+  ]
+};
+
+// Your Achievement Section Include Your Certification Talks and More
+
+const achievementSection = {
+
+  title: emoji("Achievements And Certifications 🏆 "),
+  subtitle: "Achievements, Certifications, Award Letters and Some Cool Stuff that I have done !",
+
+  achivementsCards: [
+    {
+      title: "Google Code-In Finalist",
+      subtitle: "First Pakistani to be selected as Google Code-in Finalist from 4000 students from 77 different countries.",
+      image: require("./assets/images/codeInLogo.webp"),
+      footerLink: [
+        { name: "Certification", url: "https://drive.google.com/file/d/0B7kazrtMwm5dYkVvNjdNWjNybWJrbndFSHpNY2NFV1p4YmU0/view?usp=sharing" },
+        { name: "Award Letter", url: "https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing" },
+        { name: "Google Code-in Blog", url: "https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html" }
+      ]
+    },
+    {
+      title: "Google Assistant Action",
+      subtitle: "Developed a Google Assistant Action JavaScript Guru that is available on 2 Billion devices world wide.",
+      image: require("./assets/images/googleAssistantLogo.webp"),
+      footerLink: [{ name: "View Google Assistant Action", url: "https://assistant.google.com/services/a/uid/000000100ee688ee?hl=en" }]
+    },
+
+    {
+      title: "PWA Web App Developer",
+      subtitle: "Completed Certifcation from SMIT for PWA Web App Development",
+      image: require("./assets/images/pwaLogo.webp"),
+      footerLink: [
+        { name: "Certification", url: "" },
+        { name: "Final Project", url: "https://pakistan-olx-1.firebaseapp.com/" }
+      ]
+    }
+  ]
+};
+
+// Blogs Section
+
+const blogSection = {
+
+  title: "Blogs",
+  subtitle: "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
+
+  blogs: [
+    {
+      url: "https://developers.decoded.africa/how-to-dockerize-django-application/",
+      title: "How to Dockerize a Django Application",
+      description: ""
+    },
+    {
+      url: "https://scotch.io/@adams_okode/building-a-restful-api-service-using-spring-boot",
+      title: "Building a RESTful API service using Spring-boot",
+      description: ""
+    },
+    {
+      url: "https://scotch.io/@adams_okode/spring-boot-on-vs-code444",
+      title: "Spring Boot on VS CODE",
+      description: ""
+    }
+  ]
+};
+
+// Talks Sections
+
+const talkSection = {
+  title: "TALKS",
+  subtitle: emoji("I LOVE TO SHARE MY LIMITED KNOWLEDGE AND GET A SPEAKER BADGE 😅"),
+
+  talks: [
+    {
+      title: "IoT In the Wild",
+      subtitle: "ALU African Industrial Internet Programme 2019 - Guest Speaker Session",
+      slides_url: "https://www.youtube.com/watch?v=ZWnzLzTk1EQ",
+      event_url: "https://www.youtube.com/watch?v=ZWnzLzTk1EQ"
+    }
+  ]
+};
+
+// Podcast Section
+
+const podcastSection = {
+  title: emoji("Podcast 🎙️"),
+  subtitle: "I LOVE TO TALK ABOUT MYSELF AND TECHNOLOGY",
+  // Please Provide with Your Podcast embeded Link
+  podcast: [""]
+};
+
+const contactInfo = {
+  title: emoji("Contact Me ☎️"),
+  subtitle: "Discuss a project or just want to say hi? My Inbox is open for all.",
+  number: "+254702759950",
+  email_address: "adamsokode@gmail.com"
+};
+
+//Twitter Section
+
+const twitterDetails = {
+  userName: ""//Replace "twitter" with your twitter username without @
+};
+export { greeting, socialMediaLinks, skillsSection, techStack, workExperiences, openSource, bigProjects, achievementSection, blogSection, talkSection, podcastSection, contactInfo, twitterDetails };
